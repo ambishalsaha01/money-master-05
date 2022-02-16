39c22ef4264f8b35966f3c1e6d55abff84d764ab
ambishalsaha01 <bishalsaha9989@gmail.com>
2022-02-16 13:35:30 -0800

    responsive design

--- js/script.js
+++ js/script.js
@@ -1,41 +1,47 @@
-// get income balance
+// **********get income balance**********
 function getIncomeBalance(){
     const incomeField = document.getElementById('my-income');
     const incomeFieldValue = incomeField.value;
     const parseIncome = parseFloat(incomeFieldValue);
-    // clear income balance
-    // incomeField.value = '';
     return parseIncome;
 }
-// get cost field
+// **********get cost field**********
 function getTotalcost(costField){
     const costInputField = document.getElementById(costField);
     const costInputFieldValue = costInputField.value;
     const parseCostField = parseFloat(costInputFieldValue);
-    // costInputField.value = '';
-    // return costInputField.value = parseCostField;  
     return parseCostField;
 }
 
-function getExpensexBalance(amount){
-    const expensesBalance = document.getElementById(amount);
+// **********get expenses field**********
+function getExpensexBalance(balancdeField, amount){
+    const expensesBalance = document.getElementById(balancdeField);
     const expensesBalanceInner = expensesBalance.innerText;
-    const parseexpensesBalance = parseFloat(expensesBalanceInner);
-    return parseexpensesBalance;
+    const parseExpensesBalance = parseFloat(expensesBalanceInner);
+    expensesBalance.innerText = parseExpensesBalance + amount;
 }
 
-// handle calculate btn
+// **********handle calculate btn**********
 document.getElementById('calculate-btn').addEventListener('click', function(){
     // get income
     const totalIncome = getIncomeBalance(); 
-    if(totalIncome > 0){
-        console.log('thik ase')
-    }
-    else{
-        console.log('thik nai')
-    }
+    // if(totalIncome > 0){
+    //     console.log('thik ase')
+    // }
+    // else{
+    //     console.log('thik nai')
+    // }
+
     // get cost field
     const foodCost = getTotalcost('food-cost');
     const rentCost = getTotalcost('rent-cost');
-    const Cost = getTotalcost('cloth-cost');
+    const clothCost = getTotalcost('cloth-cost');
+    const totalCost = foodCost + rentCost + clothCost;
+
+    // update expens balance
+    getExpensexBalance('total-expenses', totalCost);
+
+    // update total-balance
+    const finalBalance = totalIncome - totalCost;
+    getExpensexBalance('total-balance', finalBalance);
 })
